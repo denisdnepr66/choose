8d6590bf7891d407653a9515db7688cb8904426b
denisdnepr66 <56921743+denisdnepr66@users.noreply.github.com>
2021-07-16 17:51:12 +0200

    Initial commit

---
+++ src/main/java/com/pjatk/choose/ChooseApplication.java
@@ -0,0 +1,13 @@
+package com.pjatk.choose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChooseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjatk/choose/ChooseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjatk.choose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChooseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
